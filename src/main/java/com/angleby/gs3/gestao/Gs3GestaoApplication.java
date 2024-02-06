464e2c73e7180f16010ce0020be0bebffc8a3722
angthefirst <angvamberg@gmail.com>
2024-02-06 10:15:06 -0300

    Commit inicial

---
+++ src/main/java/com/angleby/gs3/gestao/Gs3GestaoApplication.java
@@ -0,0 +1,13 @@
+package com.angleby.gs3.gestao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gs3GestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gs3GestaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angleby/gs3/gestao/Gs3GestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angleby.gs3.gestao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gs3GestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
